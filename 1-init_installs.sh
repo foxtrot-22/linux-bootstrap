2ceaa4940486a1144a177f0ed456aa3c89aa65e5
foxtrot-22 <60078271+foxtrot-22@users.noreply.github.com>
2020-02-07 11:07:29 +1030

    Add files via upload

---
+++ 1-init_installs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y dkms openssh-server tmux htop curl apt-transport-https build-essential linux-headers-$(uname -r)
+systemctl enable ssh
+mkdir -p /media/cdrom
+mount /dev/cdrom /media/cdrom
+/media/cdrom/VBoxLinuxAdditions.run
+reboot
+
+
