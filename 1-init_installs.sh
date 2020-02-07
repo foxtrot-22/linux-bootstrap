d3863518798842a80acb8e306c98f423acedffeb
foxtrot-22 <60078271+foxtrot-22@users.noreply.github.com>
2020-02-07 12:19:36 +1030

    Update 1-init_installs.sh

--- 1-init_installs.sh
+++ 1-init_installs.sh
@@ -3,9 +3,9 @@
 apt-get update
 apt-get install -y dkms openssh-server tmux htop curl apt-transport-https build-essential linux-headers-$(uname -r)
 systemctl enable ssh
-mkdir -p /media/cdrom
-mount /dev/cdrom /media/cdrom
-/media/cdrom/VBoxLinuxAdditions.run
-reboot
+#mkdir -p /media/cdrom
+#mount /dev/cdrom /media/cdrom
+#/media/cdrom/VBoxLinuxAdditions.run
+#reboot
 
 
