872db92af35b7294c556cb116139048e6b9ef31e
=foxtrot-22 <nhey@lmg.com.au>
2020-02-07 13:23:46 +1030

    updated script for vars

--- 2-post-ga.sh
+++ 2-post-ga.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
-#apt-get update
-#echo ***Updates complete***
-#sleep 2
-#apt-get install -y dkms openssh-server tmux htop curl apt-transport-https build-essential linux-headers-$(uname -r)
-#echo ***Software installed***
-#sleep 2
-#systemctl enable ssh
-#echo ***SSH Running***
-#sleep 2
-#echo; read -rsn1 -p"Insert the guest additions CD and press any key"; echo
-#echo ***Installing Guest Additions***
-#mkdir -p /media/cdrom
-#mount /dev/cdrom /media/cdrom
-#/media/cdrom/VBoxLinuxAdditions.run
-#echo ***Vbox installed***
 usermod -G vboxsf -a $USER
 echo "Added user to boxsf"
 sleep 2
+
 # Display hostname
-echo "Existing hostname is $hostn"
+echo "Existing hostname is $HOSTNAME"
 # Ask for new hostname
 echo "Enter new hostname: "
-read newhost
+read NEWHOST
 
 #Change hostname in /etc/hosts and /etc/hostname
-sudo sed -i "s/$hostn/$newhost/g" /etc/hosts
-sudo sed -i "s/$hostn/$newhost/g" /etc/hostname
+sudo sed -i "s/$HOSTNAME/$NEWHOST/g" /etc/hosts
+sudo sed -i "s/$HOSTNAME/$NEWHOST/g" /etc/hostname
 
 # Display the hostname
-echo "Your new hostname is $newhost"
+echo "Your new hostname is $NEWHOST"
 echo; read -rsn1 -p"Press any key to reboot"
 sudo reboot
