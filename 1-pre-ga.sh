9d88c5fc7586d86b3b20697f0ba2f553e1ef7996
foxtrot-22 <nhey@lmg.com.au>
2020-02-07 13:08:55 +1030

    Finalised the 1st script

--- 1-pre-ga.sh
+++ 1-pre-ga.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 apt-get update
+echo ***Updates complete***
+sleep 2
 apt-get install -y dkms openssh-server tmux htop curl apt-transport-https build-essential linux-headers-$(uname -r)
+echo ***Software installed***
+sleep 2
+systemctl enable ssh
+echo ***SSH Running***
+sleep 2
+echo; read -rsn1 -p"Insert the guest additions CD and press any key"; echo
+echo ***Installing Guest Additions***
+mkdir -p /media/cdrom
+mount /dev/cdrom /media/cdrom
+/media/cdrom/VBoxLinuxAdditions.run
+echo ***Vbox installed***
+echo; read -rsn1 -p"Press any key to reboot"
+sudo reboot
